d98d81d61eeeee4d9fde6741137f4f7f9509c4e7
ka12114242 <aayushkhadka274@gmail.com>
2025-08-14 11:43:10 -0500

    stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Auto/Movement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Auto/Movement.java
@@ -5,8 +5,9 @@ package org.firstinspires.ftc.teamcode.pedroPathing.Auto;
 
 import org.firstinspires.ftc.teamcode.pedroPathing.Auto.Constants;
 import com.pedropathing.localization.Pose;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
+@Autonomous
 public class Movement extends LinearOpMode {
 
     private final Constants constants = new Constants();
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/teleop/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/teleop/Teleop.java
@@ -1,10 +1,23 @@
 package org.firstinspires.ftc.teamcode.pedroPathing.teleop;
 import org.firstinspires.ftc.teamcode.pedroPathing.constants.LConstants;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.robot.Robot;
 
+
+@TeleOp
 public class Teleop extends LinearOpMode {
 
+    private enum RobotState {
+        INIT,
+        INTAKE,
+        BASKET,
+        SAMPLE,
+        SPECIMEN,
+        ASCEND
+    }
 
     DcMotorEx frontLeft = null;
     DcMotorEx frontRight = null;
@@ -20,36 +33,93 @@ public class Teleop extends LinearOpMode {
         backRight = hardwareMap.get(DcMotorEx.class, "rightBack");
 
         waitForStart();
-        frontLeft.setDirection(DcMotorEx.Direction.REVERSE);
-        backLeft.setDirection(DcMotorEx.Direction.REVERSE);
+        frontRight.setDirection(DcMotorEx.Direction.REVERSE);
+        backRight.setDirection(DcMotorEx.Direction.REVERSE);
 
 
         frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE)
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
-        double x = gamepad1.left_stick_x; // Counteract imperfect strafing
-        double rx = gamepad1.right_stick_x;
+        RobotState currentState = RobotState.INIT;
 
-        double denominator = Math.max(Math.max(Math.abs(x), Math.abs(y)), rx);
 
-        if (gamepad1.left_trigger != 0 || gamepad1.right_trigger != 0) {
-            denominator = denominator*2;
+        while (opModeIsActive()){
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
+
+            double denominator = Math.max(Math.max(Math.abs(x), Math.abs(y)), rx);
+
+            if (gamepad1.left_trigger != 0 || gamepad1.right_trigger != 0) {
+                denominator = denominator*2;
+            }
+
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+
+            frontLeft.setPower(frontLeftPower);
+            backLeft.setPower(backLeftPower);
+            frontRight.setPower(frontRightPower);
+            backRight.setPower(backRightPower);
+
+
+            if (gamepad1.a){
+                currentState = RobotState.INTAKE;
+            }
+            else if ( gamepad1.b){
+                currentState = RobotState.ASCEND;
+            }
+            else if (gamepad1.x){
+                currentState = RobotState.BASKET;
+            }
+            else if (gamepad1.right_trigger > 0.1){
+                currentState = RobotState.SAMPLE;
+            }
+            else if (gamepad1.left_trigger > 0.1){
+                currentState = RobotState.SPECIMEN;
+            }
+
+            switch (currentState){
+                case INIT:
+                    telemetry.addData("State", "INIT");
+                    telemetry.update();
+                    break;
+
+                case INTAKE:
+                    telemetry.addData("State", "INTAKE");
+                    telemetry.update();
+                    break;
+
+                case ASCEND:
+                    telemetry.addData("State", "ASCEND");
+                    telemetry.update();
+                    break;
+
+                case SAMPLE:
+                    telemetry.addData("State", "SAMPLE");
+                    telemetry.update();
+                    break;
+
+                case SPECIMEN:
+                    telemetry.addData("State", "SPECIMEN");
+                    telemetry.update();
+                    break;
+
+                case BASKET:
+                    telemetry.addData("State", "BASKET");
+                    telemetry.update();
+                    break;
+            }
+
+
+
+
         }
-
-        double frontLeftPower = (y + x + rx) / denominator;
-        double backLeftPower = (y - x + rx) / denominator;
-        double frontRightPower = (y - x - rx) / denominator;
-        double backRightPower = (y + x - rx) / denominator;
-
-
-        frontLeft.setPower(frontLeftPower);
-        backLeft.setPower(backLeftPower);
-        frontRight.setPower(frontRightPower);
-        backRight.setPower(backRightPower);
-
 
 
     }
