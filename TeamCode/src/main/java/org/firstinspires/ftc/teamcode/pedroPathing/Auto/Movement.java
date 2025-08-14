a08b40069b7d066593834ab801985a73662b78e4
ka12114242 <aayushkhadka274@gmail.com>
2025-08-14 10:28:32 -0500

    Pasted movement and teleop, next steps to make constants enum

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Auto/Movement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Auto/Movement.java
@@ -1,12 +1,5 @@
 package org.firstinspires.ftc.teamcode.pedroPathing.Auto;
 
-import com.pedropathing.localization.Pose;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+public class Movement {
 
-public class Movement extends LinearOpMode {
-    private static Pose START_POS = new Pose(0,0,Math.toRadians(0));
-    public void runOpMode()
-    {
-
-    }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/teleop/Teleop.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.pedroPathing.teleop;
+import org.firstinspires.ftc.teamcode.pedroPathing.constants.LConstants;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+public class Teleop extends LinearOpMode {
+
+
+    DcMotorEx frontLeft = null;
+    DcMotorEx frontRight = null;
+    DcMotorEx backLeft = null;
+    DcMotorEx backRight = null;
+
+    public void runOpMode() throws InterruptedException{
+
+        frontLeft = hardwareMap.get(DcMotorEx.class, "leftFront");
+        frontRight = hardwareMap.get(DcMotorEx.class, "rightFront");
+        backLeft = hardwareMap.get(DcMotorEx.class, "leftBack");
+        backRight = hardwareMap.get(DcMotorEx.class, "rightBack");
+
+        waitForStart();
+        frontLeft.setDirection(DcMotorEx.Direction.REVERSE);
+        backLeft.setDirection(DcMotorEx.Direction.REVERSE);
+
+
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE)
+
+        double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+        double x = gamepad1.left_stick_x; // Counteract imperfect strafing
+        double rx = gamepad1.right_stick_x;
+
+        double denominator = Math.max(Math.max(Math.abs(x), Math.abs(y)), rx);
+
+        if (gamepad1.left_trigger != 0 || gamepad1.right_trigger != 0) {
+            denominator = denominator*2;
+        }
+
+        double frontLeftPower = (y + x + rx) / denominator;
+        double backLeftPower = (y - x + rx) / denominator;
+        double frontRightPower = (y - x - rx) / denominator;
+        double backRightPower = (y + x - rx) / denominator;
+
+
+        frontLeft.setPower(frontLeftPower);
+        backLeft.setPower(backLeftPower);
+        frontRight.setPower(frontRightPower);
+        backRight.setPower(backRightPower);
+
+
+
+    }
+
+}
