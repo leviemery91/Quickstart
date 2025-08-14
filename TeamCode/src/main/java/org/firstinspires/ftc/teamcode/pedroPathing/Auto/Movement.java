81e7953b7022f9ba3bb940ffd6b2fed87ce7bb99
leviemery91 <levi@emeryzone.com>
2025-08-14 10:01:18 -0500

    constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Auto/Movement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Auto/Movement.java
@@ -1,5 +1,12 @@
 package org.firstinspires.ftc.teamcode.pedroPathing.Auto;
 
-public class Movement {
+import com.pedropathing.localization.Pose;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+public class Movement extends LinearOpMode {
+    private static Pose START_POS = new Pose(0,0,Math.toRadians(0));
+    public void runOpMode()
+    {
+
+    }
 }
