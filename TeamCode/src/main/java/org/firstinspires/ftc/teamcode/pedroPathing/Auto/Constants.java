f6267ce90124365cb58ef8e0c37999e091207e5c
leviemery91 <levi@emeryzone.com>
2025-08-15 10:06:58 -0500

    Added more Poses

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Auto/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Auto/Constants.java
@@ -4,6 +4,10 @@ import com.pedropathing.localization.Pose;
 
 public class Constants {
     public final Pose START_POS = new Pose(0,0,Math.toRadians(0));
+    public final Pose POS_ONE = new Pose(-12,6,Math.toRadians(76));
+    public final Pose POS_TWO = new Pose(12,-6,Math.toRadians(200));
+    public final Pose POS_THREE = new Pose(24,0,Math.toRadians(76));
+    public final Pose POS_FOUR = new Pose(0,24,Math.toRadians(13));
     public final Pose END_POS = new Pose(24, 24, Math.toRadians(0));
     public final double MOVEMENT_SPEED = 0.5;
     public final double PRECISION_SPEED = 0.3;
