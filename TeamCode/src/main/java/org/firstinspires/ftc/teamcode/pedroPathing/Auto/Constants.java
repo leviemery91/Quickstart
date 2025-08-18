c2b1abd269a0d7035190f1248c067217fa844c21
leviemery91 <levi@emeryzone.com>
2025-08-18 10:56:12 -0500

    added a testing program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Auto/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Auto/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
     public final Pose START_POSE = new Pose(0,0,Math.toRadians(0));
     public final Pose POSE_ONE = new Pose(-12,6,Math.toRadians(76));
     public final Pose POSE_TWO = new Pose(12,-6,Math.toRadians(200));
-    public final Pose POSE_THREE = new Pose(24,0,Math.toRadians(76));
+    public final Pose POSE_THREE = new Pose(24,0,Math.toRadians(180));
     public final Pose POSE_FOUR = new Pose(0,24,Math.toRadians(13));
     public final Pose END_POSE = new Pose(24, 24, Math.toRadians(0));
     public final double MOVEMENT_SPEED = 0.5;
